bae7b6ddf8424c8f5ba23d1486577fb77f3bbc49
daeun <122bittersweet@gmail.com>
2022-08-30 01:03:26 +0900

    init commit

---
+++ hello-react/src/ValidationSample.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ScrollBox from './ScrollBox';
+
+class ValidationSample extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox/>
+      </div>
+    );
+  }
+}
+
+export default ValidationSample;
